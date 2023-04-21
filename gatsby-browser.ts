fd121a5d0f5c472d838f49c2d89329c8e6fe822c
Anteneh Addis <antxaddis@gmail.com>
2023-04-20 23:11:38 -0400

    Updates

--- gatsby-browser.ts
+++
@@ -1,6 +0,0 @@
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
-import '@fontsource/manrope/400.css'
-import '@fontsource/manrope/700.css'
-import '@fontsource/manrope/800.css'
